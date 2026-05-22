cd4bdda3bc4dedbe29a22bb24f95014f86359ef2
CRollins22 <claudeotieno49@gmail.com>
2026-05-22 13:51:57 +0300

    Automatic refreshing Date, Objects etc...

--- js/script.js
+++ js/script.js
@@ -1,13 +1,38 @@
-function greetings() {
-    var dayTime = new Date().getHours();
-    var hours = dayTime.getHours();
-    var greeting;
-    if (hours < 12) { 
-        greeting = "Good Morning!";
-    } else if (hourse < 18) {
-        greeting = "Good Afternoon!";
-    } else {
-        greeting = "Good Evening!";
-    }
-    return greeting + "" + fname + "!";
+function greetings(fname) {
+var dayTime = new Date();
+var hours = dayTime.getHours();
+var greeting;
+if (hours < 12) {
+greeting = "Good morning!";
+} else if (hours < 18) {
+greeting = "Good afternoon!";
+} else {
+greeting = "Good evening!";
+}
+return greeting + " " + fname + "!";
+}
+
+function starTime() {
+           // Get the current date and time
+        var today = new Date();
+
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var dayOfWeek = days[today.getDay()]; // Get the day of the week
+        var day = today.getDate();
+
+        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        var month = months[today.getMonth()]; // Months are zero-based
+
+        var year = today.getFullYear();
+
+        var hours = today.getHours();
+        var minutes = today.getMinutes();
+        var seconds = today.getSeconds();
+        document.getElementById("realTime").innerHTML = dayOfWeek + ", " + day + " " + month + " " + year + " " + formatTime(hours) + ":" + formatTime(minutes) + ":" + formatTime(seconds);
+        t=setTimeout(starTime, 1000); // Refresh the time every second
+}
+
+function formatTime(i) {
+    if (i < 10) {i = "0" + i}  // add zero in front of numbers < 10
+    return i;
 }
